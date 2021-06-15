d7216b23f7cbab0664c564bc93599d1378b1dbdb
Oumar Kane <83582338+Oumar199@users.noreply.github.com>
2021-06-15 18:02:35 +0000

    Update dessinMSDA.py

--- dessinMSDA.py
+++ dessinMSDA.py
@@ -10,9 +10,9 @@ from math import *
 from numpy.random import randint as rdt
 
 # Nous allons créer pour chaque type de figure simple (triangle, rectangle, carre, cercle, ...) 
-# une fonction (ou procédure) qui comportera les manipulation permettant de tracer la figure
+# une fonction (ou procédure) qui comportera les manipulations permettant de tracer la figure
 
-# Mais d'abord creons quelques fonctions tres utiles au programme
+# Mais d'abord créons quelques fonctions trés utiles au programme
 def set_move(x, y: float = None):
     """Cette fonction permet au turtle de quitter un endroit pour aller dans un autre sans être 
     Visible
@@ -20,7 +20,7 @@ def set_move(x, y: float = None):
         Utilisation de la fonction penup() et pendown() pour lever le crayon et ne rien tracer
         et utiliser la fonction goto() pour aller vers un endroit selon les coordonnées x et y
     Args:
-        x(float ou tuple ou list): si c'est une liste ou un tuple goto la prendra seule comme para-
+        x(float ou tuple ou list): si c'est une liste ou un tuple goto le prendra seul comme para-
         -mètre sinon goto prendra avec elle un deuxième paramètre de type float
         y(float): le deuxième paramètre de goto si x n'est pas un tuple ou une liste
     Returns: 
@@ -63,12 +63,12 @@ def al_kashi(
 
 # On implemente les fonctions permettant de créer les figures simples
 def carre(cote: float):
-    """Cette fonction nous permet de tracer une carre
+    """Cette fonction nous permet de tracer un carré
     Method:
         On utilise la boucle for pour répéter les fonctions forward et left(90 degrés). Ce qui nous
-        permet de tracer les quatre côtés du carre d'égales longueurs
+        permet de tracer les quatre côtés du carré d'égales longueurs
     Args:
-        cote(float): La longueur de chaque cote du carre. Cette longueur ne doit être inferieure ou
+        cote(float): La longueur de chaque côté du carré. Cette longueur ne doit être inferieure ou
         égale a 0
     Returns:
         None
@@ -91,7 +91,7 @@ def rectangle(
         pour le créer (calcul du périmètre)
     Args:
         longueur(float): la longueur du rectangle qui ne doit pas être inferieure ou égale a 0
-        largeur(float): la largeur du rectangle qui ne doit pas non plus être inferieure ou égale a 0
+        largeur(float): la largeur du rectangle qui ne doit pas non plus être inferieure ou égale à 0
     Returns:
         None
     """
@@ -103,7 +103,7 @@ def rectangle(
             forward(largeur)
             right(90)
     else:
-        raise ValueError("La longueur et la largeur ne doivent pas être inferieures ou egales a 0")
+        raise ValueError("La longueur et la largeur ne doivent pas être inférieures ou égales a 0")
 
 
 def cercle(rayon: float):
@@ -123,12 +123,12 @@ def cercle(rayon: float):
 def ellipse(rayon: float):
     """Cette fonction permet de tracer une ellipse
     Method:
-        On utilise la boucle for pour répéter deux fois les commandes tracer un quart de cercle de rayon donné en para-
-        -mètre et un quart de cercle de rayon de longueur faisant la partie entière de la longueur du rayon en paramètre 
+        On utilise la boucle for pour répéter deux fois les commandes "tracer un quart de cercle de rayon donné en para-
+        -mètre et un quart de cercle de rayon dont la longueur est la partie entière de la longueur du rayon donné en paramètre 
         divisée par deux. Ces tracées de quart de cercle se feront à l'aide de la fonction circle de turtle
     Args:
-        rayon(float): la longueur du rayon utilisé deux fois pour tracer le quart du cercle. La longueur du rayon doit être
-        supérieure a 2 pour éviter que la partie entière de la longueur du rayon divisée par deux soit égale 0
+        rayon(float): la longueur du rayon utilisé sur deux quarts du cercle. La longueur du rayon doit être
+        supérieure à 2 pour éviter que la partie entière de la longueur du rayon divisée par deux soit égale 0
     Returns:
         None
     """
@@ -159,21 +159,21 @@ def triangle(
     cote_3: float, up: bool = True):
     """Cette fonction permet de tracer un triangle quelconque
     Method:
-        On récupère l'angle droit et l'angle a l'opposé de la base à l'aide de la fonction d'Al-Kachi. On vérifie ensuite si le
+        On récupère l'angle droit et l'angle à l'opposé de la base du triangle à l'aide de la fonction d'Al-Kachi. On vérifie ensuite si le
         paramètre "up" indique True ou False pour savoir si l'on doit utiliser la fonction left oubien la fonction right pour 
         tourner la tortue. 
         Dans les deux cas on va suivre ce procédé :
         Tracer le premier coté du triangle avec forward(cote_1), puis tourner a (180 - angle) (angle droit du triangle);
-        On trace le deuxième coté avec forward(cote_2), puis tourner a 180 - angle2 (angle a l'oppose de la base) et enfin
+        On trace le deuxième coté avec forward(cote_2), puis tourner a 180 - angle2 (angle à l'opposé de la base) et enfin
         On trace le troisième côté avec forward(cote_3).
     Args:
-        cote_1(float): la longueur de la base du triangle qui doit être supérieure a 0 et inferieure a la somme des deux autres côtés
+        cote_1(float): la longueur de la base du triangle qui doit être supérieure à 0 et inférieure à la somme des deux autres côtés
         du triangle
-        cote_2(float): la longueur du cote droit du triangle qui doit être supérieure a 0 et inferieure a la somme des deux autres côtés
-        cote_3(float): la longueur du cote gauche du triangle qui doit être supérieure a 0 et inferieure a la somme des deux autres
+        cote_2(float): la longueur du côté droit du triangle qui doit être supérieure à 0 et inferieure à la somme des deux autres côtés
+        cote_3(float): la longueur du côté gauche du triangle qui doit être supérieure à 0 et inferieure à la somme des deux autres
         côtés
-        up(bool): la valeur booléenne qui doit indiquer si le sommet du triangle pointe vers le côté supérieur de la base ou vers 
-        son côté inferieur. Si elle n'est pas indiquée alors elle prendra par defaut la valeur True
+        up(bool): la valeur booléenne qui doit indiquer si le sommet du triangle pointe vers le côté supérieur à la base ou vers 
+        le côté inférieur à la base du trapèze. Si elle n'est pas indiquée alors elle prendra par défaut la valeur True
     Returns: 
         None
     """
@@ -203,13 +203,13 @@ def triangle(
 def losange(cote: float, diag = rdt(10, 50)):
     """Cette fonction permet de tracer un losange
     Method:
-        On a décider de tracer deux triangles de base commune (la diagonale indiquée en paramètre
+        On a décider de tracer deux triangles de base commune (la diagonale indiquée en paramètre)
         Utiliser la fonction d'Al-Kachi pour trouver les angles de chaque triangle
-        Pour concevoir les deux triangles on va tourner a 180 - angle degrés (angle droit de chaque triangle), avancer avec
+        Pour concevoir les deux triangles on va tourner à 180 - angle degrés (angle droit de chaque triangle), avancer avec
         forward(cote), tourner a 180 - angle2 degrés (angle à l'opposé du diagonale du losange) et avancer avec forward(cote)
-        Les différences résident aux points suivant pour chaque triangle :
+        Les différences résident aux points suivants pour chaque triangle :
         - triangle 1: on lève le crayon avec penpup(), on avance avec forward(diag) et on remet le crayon en place (pendown())
-        - triangle 2: on tourne a "angle" degrés à droite
+        - triangle 2: on tourne à "angle" degrés à droite
     Args:
         cote(float): la longueur de chaque côté du losange qui doit être positive
         diag(flaot ou int): la longueur de la diagonale du losange qui doit être positive. Si elle n'est pas donnée alors elle
@@ -240,16 +240,18 @@ def trapeze(cote_1: float, cote_2: float, cote_3 = rdt(20, 40), angle: float = 9
         Si "up" est à True, on utilise la fonction left(angle en degrés) pour tourner la tortue
         sinon on utilise right(angle en degrés) pour la tourner
         On avance avec forward(longueur base du trapèze), on tourne a 180 - angle (angle droit du trapèze) degrés, on avance 
-        avec forward(longueur côté droit du trapèze), on tourne à angle (angle droit du trapèze) degrés pour ensuite avancer avec                   forward(longueur côté parallèle a la base) pour enfin avancer le crayon jusqu'à la position initiale avec goto(position ini-
+        avec forward(longueur côté droit du trapèze), on tourne à angle (angle droit du trapèze) degrés pour ensuite avancer avec                   
+        forward(longueur côté parallèle à la base) pour enfin avancer le crayon jusqu'à la position initiale avec goto(position ini-
         -tiale)
     Args:
-        cote_1(float): la longueur de la base du trapèze qui doit être supérieure a la longueur du côté qui lui est opposé et
-        supérieure a 0
-        cote_2(float): la longueur du côté du trapèze qui doit être supérieure a 0
-        cote_3(float): la longueur du côté opposé à la base qui doit être supérieure a 0 et a la longueur de la base. Si cette longueur
+        cote_1(float): la longueur de la base du trapèze qui doit être supérieure à la longueur du côté qui lui est opposé et
+        supérieure à 0
+        cote_2(float): la longueur du côté du trapèze qui doit être supérieure à 0
+        cote_3(float): la longueur du côté opposé à la base qui doit être supérieure à 0 et à la longueur de la base. Si cette longueur
         n'est pas précisée alors sa valeur entière sera comprise entre 20 et 40
         angle(float): angle au bas-droit du trapèze. Sa valeur doit être comprise entre 20 et 160 degrés
-        up(bool): la valeur booléenne qui doit indiquer si le côté opposé de la base du trapèze sera conçu vers le côté supérieur de la             base du trapèze ou vers son côté inferieur. S'il n'est pas indiquée alors elle prendra par défaut la valeur True 
+        up(bool): la valeur booléenne qui doit indiquer si le côté opposé de la base du trapèze sera conçu vers le côté supérieur à la             
+        base du trapèze ou vers le côté inférieur à la base du trapèze. Si elle n'est pas indiquée alors elle prendra par défaut la valeur True 
     Returns:
         None
     """
