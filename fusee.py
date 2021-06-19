bc52c1e96b6cc0026b945e99b1f528a0f94e7805
Oumar Kane <83582338+Oumar199@users.noreply.github.com>
2021-06-19 00:31:52 +0000

    Update fusee.py

--- fusee.py
+++ fusee.py
@@ -13,31 +13,33 @@ def fusee():
     
     #On va utiliser un dictionnaires pour stocker les positions (simple precaution)
     positions = {}
-    #Tracons le premier triangle et stockons sa position avant pour pouvoir l'utiliser apres
-    #Les longueurs des figures seront stockees dans des variables comme suivants
+    #Tracons le premier triangle et stockons sa position pour pouvoir l'utiliser plus tard
+    #Les longueurs des figures seront stockees dans des variables 
     ltri1 = 66
     set_move(pos()[0] - 33, pos()[1] + 200)
     positions["tri1"] = pos()
     fillcolor("red"); begin_fill(); triangle(ltri1, ltri1, ltri1); end_fill()
     setheading(0)
     
-    #On reprend la position de depart pour tracer un carre rouge au milieu du triangle et stockage de sa position
+    #On reprend la position de depart pour tracer un carre rouge au milieu du triangle et on stocke sa position (position du 
+    #curseur plus precisement)
     set_move(positions["tri1"][0] + 18, positions["tri1"][1])
     lcar1 = (ltri1 - 6) / 2
     fillcolor("red"); begin_fill(); carre(lcar1); end_fill()
     positions["carre1"] = pos()
     
-    #Tracons un carre juste en dessous apres avoir defini une position suivant la position
+    #Tracons un carre juste en dessous du premier carre apres avoir defini une position du curseur suivant la position
     #retenue du carre precedent
     lcar2 = ltri1 - 8*2
     set_move(positions["tri1"][0] + 8, positions["tri1"][1] - lcar2) 
     setheading(0)
     #On utilisera la methode du fillcolor(remplir couleur);begin_fill(commencer le remplissage)
     #fonction(la fonction nous permettant de tracer une figure); end_fill(finir le remplissage)
+    #pour tracer toutes les figures de cette fonction
     fillcolor("lightgreen"); begin_fill(); carre(lcar2); end_fill()
     #On stocke egalement cette position
     positions["carre2"] = pos()
-    # Le triangle suivant comporte un côté de longueur egale a car2 un autre de longueur egale a car2/2 et un autre de longueur inconnu
+    # Le triangle suivant comporte un côté de longueur egale a car2, un autre de longueur egale a car2/2 et un autre de longueur inconnue
     # calculons cette longueur avec le theoreme de al-kachi (Ce theoreme nous servira tout au long du programme)
     lcote1_t2 = lcar2
     lcote2_t2 = lcar2/2
@@ -54,7 +56,7 @@ def fusee():
     lrec1 = lcar2 * 2
     Lrec1 = 20
     # A preciser qu'a chaque fois qu'on souhaite avoir un angle bien precis 
-    # Pour tracer on va utiliser la fonction setheading(angle)
+    # on va utiliser la fonction setheading(angle). 
     setheading(0)
     #Tracons le rectangle et recuperons sa position plus tard
     fillcolor("lightblue"); begin_fill(); rectangle(lrec1, Lrec1); end_fill() 
@@ -83,14 +85,14 @@ def fusee():
     fillcolor("grey"); begin_fill(); carre(lcote2_rc1); end_fill()
     positions["carre3"] = pos() # recuperation de la position du dernier carre
     
-    #On trace un rectangle a cote des carres de bleu foncé (on souhaite que l'on ai l'impression
-    # de voir des ombres la fusee
+    #On trace un rectangle, a cote des carres, de couleur bleu foncé (on souhaite que avoir l'impression
+    # d'avoir des ombres sur la fusee
     set_move(positions["rec1"][0], positions["rec1"][1] - Lrec1)
     setheading(0)
     fillcolor("darkblue"); begin_fill(); rectangle(lrec1, lcote2_rc1); end_fill() 
     positions["rec2"] = pos()
     
-    #La fonction set_move() sera utilisé a plusieurs et semble etre l'une des fonctions les plus utiles
+    #La fonction set_move() sera utilisé a plusieurs reprises et semble d'ailleur etre l'une des fonctions les plus utiles
     #de ce projet
     set_move(pos()[0] - lcote2_rc1, pos()[1] - lcote2_rc1)
     #On trace deux rectangles de couleur orange pour indiquer les flammes sortant de la fusee
